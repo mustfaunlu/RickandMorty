d68cc08e362d7f153ed597cc02cbbc6551517352
unlu <corepavlion@gmail.com>
2023-03-31 17:58:01 +0300

    Added Constants.kt

---
+++ app/src/main/java/com/unludev/rickandmorty/common/Constants.kt
@@ -0,0 +1,8 @@
+package com.unludev.rickandmorty.common
+
+object Constants {
+    const val GONE_AND_NO_MORE_DATA = 0
+    const val VISIBLE_AND_DELAYED = 1
+    const val VISIBLE_AND_LOADING = 2
+    const val VISIBLE_AND_ERROR = -1
+}
